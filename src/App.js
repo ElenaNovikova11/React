50316000da887cc8523a72b94d28e618e0b96d12
elena <princessa-len4ik@mail.ru>
2026-01-31 19:37:45 +0500

    SaveALL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/main/Header';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Contacts from './page/Contacts';
 import Portfolio from './page/Portfolio';
@@ -15,12 +15,19 @@ function App() {
     <BrowserRouter>
       <div>
         <Header />
-        <Routes>
-          <Route path='/Contacts' name='Contacts' element = {<Contacts />}/>
+        
+       
+    <Routes>
+        <Route path='/Contacts' name='Contacts' element = {<Contacts />}/>
           <Route path='/Portfolio' name='Portfolio' element = {<Portfolio />}/>
           <Route path='/AboutMy' name='AboutMy' element = {<AboutMy />}/>
-          <Route exact path="/" name='Home' element = {<Home />}/>
-        </Routes>
+          <Route path="/React" name='Home' element = {<Home />} />
+          <Route exact path="/" name='Home' element = {<Home />} />
+    </Routes>
+        
+       
+         
+       
 
       </div>
 
